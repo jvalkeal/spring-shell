fed97dad2f91e8242ef6ccb57908bb345cc666d7
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-03-23 08:22:39 +0000

    stash

--- spring-shell-core/src/test/java/org/springframework/shell/command/CommandParserTests.java
+++ spring-shell-core/src/test/java/org/springframework/shell/command/CommandParserTests.java
@@ -35,7 +35,7 @@ public class CommandParserTests extends AbstractCommandTests {
 	}
 
 	@Test
-	public void testOptionNameToArg() {
+	public void testLongName() {
 		CommandOption option1 = CommandOption.of(new String[]{"arg1"}, new Character[0], "desc");
 		CommandOption option2 = CommandOption.of(new String[]{"arg2"}, new Character[0], "desc");
 		List<CommandOption> options = Arrays.asList(option1, option2);
@@ -46,15 +46,15 @@ public class CommandParserTests extends AbstractCommandTests {
 		assertThat(results.results().get(0).value()).isEqualTo("foo");
 	}
 
-	// @Test
-	// public void testOptionAliasToArg() {
-	// 	CommandOption option1 = CommandOption.of("arg1", new String[]{"--arg11"}, "desc");
-	// 	CommandOption option2 = CommandOption.of("arg2", new String[]{"--arg22"}, "desc");
-	// 	List<CommandOption> options = Arrays.asList(option1, option2);
-	// 	String[] args = new String[]{"--arg22", "foo"};
-	// 	Results results = parser.parse(options, args);
-	// 	assertThat(results.results()).hasSize(1);
-	// 	assertThat(results.results().get(0).option()).isSameAs(option2);
-	// 	assertThat(results.results().get(0).value()).isEqualTo("foo");
-	// }
+	@Test
+	public void testShortName() {
+		CommandOption option1 = CommandOption.of(new String[0], new Character[]{'a'}, "desc");
+		CommandOption option2 = CommandOption.of(new String[0], new Character[]{'b'}, "desc");
+		List<CommandOption> options = Arrays.asList(option1, option2);
+		String[] args = new String[]{"-a", "foo"};
+		Results results = parser.parse(options, args);
+		assertThat(results.results()).hasSize(1);
+		assertThat(results.results().get(0).option()).isSameAs(option1);
+		assertThat(results.results().get(0).value()).isEqualTo("foo");
+	}
 }
