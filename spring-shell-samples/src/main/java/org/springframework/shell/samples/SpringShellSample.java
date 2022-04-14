cd475ec85e3b3c297683502f721f2eadd06d15a0
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-04-14 10:14:27 +0100

    stash

--- spring-shell-samples/src/main/java/org/springframework/shell/samples/SpringShellSample.java
+++ spring-shell-samples/src/main/java/org/springframework/shell/samples/SpringShellSample.java
@@ -22,7 +22,6 @@ import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.shell.jline.PromptProvider;
 
 /**
