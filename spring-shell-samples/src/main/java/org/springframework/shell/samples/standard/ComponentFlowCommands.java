c43e4efe117261df843459f215fc049e03ab9814
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-06-14 07:22:10 +0100

    stash

--- spring-shell-samples/src/main/java/org/springframework/shell/samples/standard/ComponentFlowCommands.java
+++ spring-shell-samples/src/main/java/org/springframework/shell/samples/standard/ComponentFlowCommands.java
@@ -24,10 +24,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.command.CommandExecution;
+import org.springframework.shell.command.CommandParser;
+import org.springframework.shell.command.CommandRegistration;
 import org.springframework.shell.component.flow.ComponentFlow;
+import org.springframework.shell.component.flow.ComponentFlow.ComponentFlowResult;
 import org.springframework.shell.component.flow.ResultMode;
 import org.springframework.shell.component.flow.SelectItem;
-import org.springframework.shell.component.flow.ComponentFlow.ComponentFlowResult;
 import org.springframework.shell.standard.AbstractShellComponent;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -131,6 +135,107 @@ public class ComponentFlowCommands extends AbstractShellComponent {
 			buf.append("\n");
 		});
 		return buf.toString();
+	}
+
+	@Bean
+	public CommandRegistration showcaseRegistration() {
+		return CommandRegistration.builder()
+			.command("flow", "showcase3")
+			.description("Showcase")
+			.withOption()
+				.longNames("field1")
+				.and()
+			.withOption()
+				.longNames("field2")
+				.and()
+			.withOption()
+				.longNames("confirmation1")
+				.type(Boolean.class)
+				.and()
+			.withOption()
+				.longNames("path1")
+				.and()
+			.withOption()
+				.longNames("single1")
+				.and()
+			.withOption()
+				.longNames("multi1")
+				.and()
+			.withTarget()
+				.consumer(ctx -> {
+					String field1 = ctx.getOptionValue("field1");
+					String field2 = ctx.getOptionValue("field2");
+					Boolean confirmation1 = ctx.getOptionValue("confirmation1");
+					String path1 = ctx.getOptionValue("path1");
+					String single1 = ctx.getOptionValue("single1");
+					List<String> multi1 = ctx.getOptionValue("multi1") != null ? Arrays.asList(ctx.getOptionValue("multi1")) : new ArrayList<>();
+					Map<String, String> single1SelectItems = new HashMap<>();
+					single1SelectItems.put("key1", "value1");
+					single1SelectItems.put("key2", "value2");
+					List<SelectItem> multi1SelectItems = Arrays.asList(SelectItem.of("key1", "value1"),
+							SelectItem.of("key2", "value2"), SelectItem.of("key3", "value3"));
+					ComponentFlow flow = componentFlowBuilder.clone().reset()
+							.withStringInput("field1")
+								.name("Field1")
+								.defaultValue("defaultField1Value")
+								.resultValue(field1)
+								.resultMode(ResultMode.ACCEPT)
+								.and()
+							.withStringInput("field2")
+								.name("Field2")
+								.resultValue(field2)
+								.resultMode(ResultMode.ACCEPT)
+								.and()
+							.withConfirmationInput("confirmation1")
+								.name("Confirmation1")
+								.resultValue(confirmation1)
+								.resultMode(ResultMode.ACCEPT)
+								.and()
+							.withPathInput("path1")
+								.name("Path1")
+								.resultValue(path1)
+								.resultMode(ResultMode.ACCEPT)
+								.and()
+							.withSingleItemSelector("single1")
+								.name("Single1")
+								.selectItems(single1SelectItems)
+								.resultValue(single1)
+								.resultMode(ResultMode.ACCEPT)
+								.and()
+							.withMultiItemSelector("multi1")
+								.name("Multi1")
+								.selectItems(multi1SelectItems)
+								.resultValues(multi1)
+								.resultMode(ResultMode.ACCEPT)
+								.and()
+							.build();
+					ComponentFlowResult result = flow.run();
+
+					List<CommandParser.CommandParserException> xxx = new ArrayList<>();
+					result.getContext().stream().forEach(e -> {
+						if (e.getValue() == null) {
+							xxx.add(CommandParser.CommandParserException.of(e.getKey() + " must be defined"));
+						}
+					});
+
+					if (xxx.isEmpty()) {
+						StringBuilder buf = new StringBuilder();
+						result.getContext().stream().forEach(e -> {
+							buf.append(e.getKey());
+							buf.append(" = ");
+							buf.append(e.getValue());
+							buf.append("\n");
+						});
+						ctx.getTerminal().writer().print(buf.toString());
+						ctx.getTerminal().writer().flush();
+					}
+					else {
+						throw new CommandExecution.CommandParserExceptionsException("ddd", xxx);
+					}
+
+				})
+				.and()
+			.build();
 	}
 
 	@ShellMethod(key = "flow conditional", value = "Second component based on first", group = "Flow")
