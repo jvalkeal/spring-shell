1754eaf504564492b1b25d9d61cd0a4df83d7774
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-06-17 16:39:19 +0100

    stash

--- spring-shell-standard/src/main/java/org/springframework/shell/standard/ValueProvider.java
+++ spring-shell-standard/src/main/java/org/springframework/shell/standard/ValueProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.shell.standard;
 
 import java.util.List;
@@ -21,15 +20,19 @@ import java.util.List;
 import org.springframework.core.MethodParameter;
 import org.springframework.shell.CompletionContext;
 import org.springframework.shell.CompletionProposal;
+import org.springframework.shell.command.CommandOption;
 
 /**
  * Beans implementing this interface are queried during TAB completion to gather possible values of a parameter.
  *
  * @author Eric Bottard
+ * @author Janne Valkealahti
  */
 public interface ValueProvider {
 
-	boolean supports(MethodParameter parameter, CompletionContext completionContext);
+	// boolean supports(CommandOption option, CompletionContext completionContext);
+	// List<CompletionProposal> complete(CommandOption option, CompletionContext completionContext);
 
+	boolean supports(MethodParameter parameter, CompletionContext completionContext);
 	List<CompletionProposal> complete(MethodParameter parameter, CompletionContext completionContext, String[] hints);
 }
