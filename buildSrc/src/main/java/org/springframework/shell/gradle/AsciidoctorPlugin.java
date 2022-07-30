0b2468443870dde9761627f9f93887dc02030ec6
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-07-30 15:19:49 +0100

    stash

--- buildSrc/src/main/java/org/springframework/shell/gradle/AsciidoctorPlugin.java
+++ buildSrc/src/main/java/org/springframework/shell/gradle/AsciidoctorPlugin.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.shell.gradle;
 
 import java.io.File;
@@ -13,14 +28,12 @@ import java.util.function.Consumer;
 
 import org.asciidoctor.gradle.base.AsciidoctorAttributeProvider;
 import org.asciidoctor.gradle.jvm.AbstractAsciidoctorTask;
-import org.asciidoctor.gradle.jvm.AsciidoctorJExtension;
 import org.asciidoctor.gradle.jvm.AsciidoctorJPlugin;
 import org.asciidoctor.gradle.jvm.AsciidoctorTask;
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.DependencySet;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.file.CopySpec;
 import org.gradle.api.file.FileTree;
