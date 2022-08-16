ce6b5dbcfa1cb04681329870d745a857e69f6c82
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-08-16 07:48:23 +0100

    stash

--- buildSrc/src/main/java/org/springframework/shell/gradle/SpringMavenPlugin.java
+++ buildSrc/src/main/java/org/springframework/shell/gradle/SpringMavenPlugin.java
@@ -21,6 +21,13 @@ import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.plugins.PluginManager;
 import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenPom;
+import org.gradle.api.publish.maven.MavenPomDeveloperSpec;
+import org.gradle.api.publish.maven.MavenPomIssueManagement;
+import org.gradle.api.publish.maven.MavenPomLicenseSpec;
+import org.gradle.api.publish.maven.MavenPomOrganization;
+import org.gradle.api.publish.maven.MavenPomScm;
+import org.gradle.api.publish.maven.MavenPublication;
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 
 public class SpringMavenPlugin implements Plugin<Project> {
@@ -32,22 +39,62 @@ public class SpringMavenPlugin implements Plugin<Project> {
 		pluginManager.apply(PublishLocalPlugin.class);
 		pluginManager.apply(PublishAllJavaComponentsPlugin.class);
 
-		project.getPlugins().withType(MavenPublishPlugin.class).all((mavenPublish) -> {
-				// PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
-				// if (project.hasProperty("deploymentRepository")) {
-				// 		publishing.getRepositories().maven((mavenRepository) -> {
-				// 				mavenRepository.setUrl(project.property("deploymentRepository"));
-				// 				mavenRepository.setName("deployment");
-				// 		});
-				// }
-				// publishing.getPublications().withType(MavenPublication.class)
-				// 				.all((mavenPublication) -> customizeMavenPublication(mavenPublication, project));
-				project.getPlugins().withType(JavaPlugin.class).all((javaPlugin) -> {
-						JavaPluginExtension extension = project.getExtensions().getByType(JavaPluginExtension.class);
-						extension.withJavadocJar();
-						extension.withSourcesJar();
-				});
+		project.getPlugins().withType(MavenPublishPlugin.class).all(mavenPublish -> {
+			PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
+			publishing.getPublications().withType(MavenPublication.class)
+				.all(mavenPublication -> customizeMavenPublication(mavenPublication, project));
+			project.getPlugins().withType(JavaPlugin.class).all(javaPlugin -> {
+				JavaPluginExtension extension = project.getExtensions().getByType(JavaPluginExtension.class);
+				extension.withJavadocJar();
+				extension.withSourcesJar();
+			});
 		});
+	}
 
+	private void customizeMavenPublication(MavenPublication publication, Project project) {
+		customizePom(publication.getPom(), project);
+	}
+
+	private void customizePom(MavenPom pom, Project project) {
+		pom.getUrl().set("https://spring.io/projects/spring-shell");
+		pom.getName().set(project.provider(project::getName));
+		pom.getDescription().set(project.provider(project::getDescription));
+		pom.organization(this::customizeOrganization);
+		pom.licenses(this::customizeLicences);
+		pom.developers(this::customizeDevelopers);
+		pom.scm(scm -> customizeScm(scm, project));
+		pom.issueManagement(this::customizeIssueManagement);
+	}
+
+	private void customizeOrganization(MavenPomOrganization organization) {
+		organization.getName().set("Pivotal Software, Inc.");
+		organization.getUrl().set("https://spring.io");
+	}
+
+	private void customizeLicences(MavenPomLicenseSpec licences) {
+		licences.license(licence -> {
+			licence.getName().set("Apache License, Version 2.0");
+			licence.getUrl().set("https://www.apache.org/licenses/LICENSE-2.0");
+		});
+	}
+
+	private void customizeDevelopers(MavenPomDeveloperSpec developers) {
+		developers.developer((developer) -> {
+			developer.getName().set("Pivotal");
+			developer.getEmail().set("info@pivotal.io");
+			developer.getOrganization().set("Pivotal Software, Inc.");
+			developer.getOrganizationUrl().set("https://www.spring.io");
+		});
+	}
+
+	private void customizeScm(MavenPomScm scm, Project project) {
+		scm.getConnection().set("scm:git:git://github.com/spring-projects/spring-shell.git");
+		scm.getDeveloperConnection().set("scm:git:ssh://git@github.com/spring-projects/spring-shell.git");
+		scm.getUrl().set("https://github.com/spring-projects/spring-shell");
+	}
+
+	private void customizeIssueManagement(MavenPomIssueManagement issueManagement) {
+		issueManagement.getSystem().set("GitHub");
+		issueManagement.getUrl().set("https://github.com/spring-projects/spring-shell/issues");
 	}
 }
