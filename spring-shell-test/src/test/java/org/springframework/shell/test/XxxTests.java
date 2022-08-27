ba7740b2b6f5db7d7dbb6e1230232e87890209b5
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-08-27 17:14:55 +0100

    stash

--- spring-shell-test/src/test/java/org/springframework/shell/test/XxxTests.java
+++ spring-shell-test/src/test/java/org/springframework/shell/test/XxxTests.java
@@ -2,7 +2,7 @@ package org.springframework.shell.test;
 
 import java.io.IOException;
 
-import com.jediterm.xxx.TestSession;
+import com.jediterm.support.TestTerminalSession;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,7 +11,7 @@ public class XxxTests {
 
 	@Test
 	void xxx1() throws IOException {
-		TestSession session = new TestSession(30, 3);
+		TestTerminalSession session = new TestTerminalSession(30, 3);
 		session.process("hi\u001B[1Dx");
 		assertThat(session.getTerminal().getTextBuffer().getScreenLines().trim()).isEqualTo("hx");
 	}
