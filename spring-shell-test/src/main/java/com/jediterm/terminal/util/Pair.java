c76723877e34769a08add654da777287242268fe
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-09-10 08:07:16 +0100

    stash

--- spring-shell-test/src/main/java/com/jediterm/terminal/util/Pair.java
+++ spring-shell-test/src/main/java/com/jediterm/terminal/util/Pair.java
@@ -1,77 +1,63 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.jediterm.terminal.util;
 
 public class Pair<A, B> {
-  public final A first;
-  public final B second;
 
-  public static <A, B> Pair<A, B> create(A first, B second) {
-    return new Pair<A, B>(first, second);
-  }
+	public final A first;
+	public final B second;
 
-  public static <T> T getFirst(Pair<T, ?> pair) {
-    return pair != null ? pair.first : null;
-  }
+	public static <A, B> Pair<A, B> create(A first, B second) {
+		return new Pair<A, B>(first, second);
+	}
 
-  public static <T> T getSecond(Pair<?, T> pair) {
-    return pair != null ? pair.second : null;
-  }
+	public static <T> T getFirst(Pair<T, ?> pair) {
+		return pair != null ? pair.first : null;
+	}
 
-  @SuppressWarnings("unchecked")
-  private static final Pair EMPTY = create(null, null);
+	public static <T> T getSecond(Pair<?, T> pair) {
+		return pair != null ? pair.second : null;
+	}
 
-  @SuppressWarnings("unchecked")
-  public static <A, B> Pair<A, B> empty() {
-    return EMPTY;
-  }
+	@SuppressWarnings("unchecked")
+	private static final Pair EMPTY = create(null, null);
 
-  public Pair(A first, B second) {
-    this.first = first;
-    this.second = second;
-  }
+	@SuppressWarnings("unchecked")
+	public static <A, B> Pair<A, B> empty() {
+		return EMPTY;
+	}
 
-  public final A getFirst() {
-    return first;
-  }
+	public Pair(A first, B second) {
+		this.first = first;
+		this.second = second;
+	}
 
-  public final B getSecond() {
-    return second;
-  }
+	public final A getFirst() {
+		return first;
+	}
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+	public final B getSecond() {
+		return second;
+	}
 
-    Pair pair = (Pair)o;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
 
-    if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
-    if (second != null ? !second.equals(pair.second) : pair.second != null) return false;
+		Pair pair = (Pair)o;
 
-    return true;
-  }
+		if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+		if (second != null ? !second.equals(pair.second) : pair.second != null) return false;
 
-  public int hashCode() {
-    int result = first != null ? first.hashCode() : 0;
-    result = 31 * result + (second != null ? second.hashCode() : 0);
-    return result;
-  }
+		return true;
+	}
 
-  public String toString() {
-    return "<" + first + "," + second + ">";
-  }
+	public int hashCode() {
+		int result = first != null ? first.hashCode() : 0;
+		result = 31 * result + (second != null ? second.hashCode() : 0);
+		return result;
+	}
+
+	public String toString() {
+		return "<" + first + "," + second + ">";
+	}
 }
