5c1a897af56b61650412ed106dfc8ca8ed5e4a34
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-09-10 08:11:25 +0100

    stash

--- spring-shell-test/src/main/java/com/jediterm/terminal/ui/TerminalPanelListener.java
+++ spring-shell-test/src/main/java/com/jediterm/terminal/ui/TerminalPanelListener.java
@@ -3,7 +3,7 @@ package com.jediterm.terminal.ui;
 import com.jediterm.terminal.RequestOrigin;
 
 public interface TerminalPanelListener {
-  void onPanelResize(RequestOrigin origin);
+	void onPanelResize(RequestOrigin origin);
 
-  void onTitleChanged(String title);
+	void onTitleChanged(String title);
 }
--- spring-shell-test/src/main/java/com/jediterm/terminal/ui/TerminalWidget.java
+++ spring-shell-test/src/main/java/com/jediterm/terminal/ui/TerminalWidget.java
@@ -1,9 +1,5 @@
 package com.jediterm.terminal.ui;
 
-import java.awt.Dimension;
-
-import javax.swing.JComponent;
-
 import com.jediterm.terminal.TerminalDisplay;
 import com.jediterm.terminal.TtyConnector;
 
@@ -11,19 +7,12 @@ import com.jediterm.terminal.TtyConnector;
  * @author traff
  */
 public interface TerminalWidget {
+
 	JediTermWidget createTerminalSession(TtyConnector ttyConnector);
-
-	// JComponent getComponent();
-
-	// default JComponent getPreferredFocusableComponent() {
-	// 	return getComponent();
-	// }
 
 	boolean canOpenSession();
 
 	void setTerminalPanelListener(TerminalPanelListener terminalPanelListener);
-
-	// Dimension getPreferredSize();
 
 	TerminalSession getCurrentSession();
 
