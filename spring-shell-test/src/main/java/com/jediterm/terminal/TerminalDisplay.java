f344c1a9520d24742a4bf3b637f6aac239f3235d
Janne Valkealahti <janne.valkealahti@gmail.com>
2022-10-19 12:53:53 +0100

    stash

--- spring-shell-test/src/main/java/com/jediterm/terminal/TerminalDisplay.java
+++ spring-shell-test/src/main/java/com/jediterm/terminal/TerminalDisplay.java
@@ -1,7 +1,5 @@
 package com.jediterm.terminal;
 
-// import com.jediterm.terminal.model.TerminalSelection;
-
 /**
  *
  * @author jediterm authors
@@ -12,28 +10,13 @@ public interface TerminalDisplay {
 
 	int getColumnCount();
 
-	// void setCursor(int x, int y);
-
-	// void setCursorShape(CursorShape shape);
-
 	void beep();
 
-	// void requestResize(Dimension newWinSize, RequestOrigin origin, int cursorX, int cursorY,
-	// 		JediTerminal.ResizeHandler resizeHandler);
-
 	void scrollArea(final int scrollRegionTop, final int scrollRegionSize, int dy);
-
-	// void setCursorVisible(boolean shouldDrawCursor);
-
-	// void setScrollingEnabled(boolean enabled);
-
-	// void setBlinkingCursor(boolean enabled);
 
 	String getWindowTitle();
 
 	void setWindowTitle(String name);
-
-	// TerminalSelection getSelection();
 
 	boolean ambiguousCharsAreDoubleWidth();
 
