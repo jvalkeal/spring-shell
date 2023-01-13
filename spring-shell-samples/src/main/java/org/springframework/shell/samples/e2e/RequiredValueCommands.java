73aad5c2fdab55fa457e7fe60bdb202445cb4fb8
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-01-13 20:48:50 +0000

    stash

--- spring-shell-samples/src/main/java/org/springframework/shell/samples/e2e/RequiredValueCommands.java
+++ spring-shell-samples/src/main/java/org/springframework/shell/samples/e2e/RequiredValueCommands.java
@@ -17,41 +17,68 @@ package org.springframework.shell.samples.e2e;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.shell.command.CommandRegistration;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.Option;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import org.springframework.stereotype.Component;
 
 /**
  * Commands used for e2e test.
  *
  * @author Janne Valkealahti
  */
-@ShellComponent
-public class RequiredValueCommands extends BaseE2ECommands {
+public class RequiredValueCommands {
 
-	@ShellMethod(key = LEGACY_ANNO + "required-value", group = GROUP)
-	public String testRequiredValueAnnotation(
-		@ShellOption(help = "Desc arg1") String arg1
-	) {
-		return "Hello " + arg1;
+	private final static String REQUIRED_VALUE = "required-value";
+	private final static String ARG1_DESC = "Desc arg1";
+	private final static String ARG1_NAME = "arg1";
+
+	@ShellComponent
+	public static class RequiredValueCommandsLegacyAnnotation extends BaseE2ECommands {
+
+		@ShellMethod(key = LEGACY_ANNO + REQUIRED_VALUE, group = GROUP)
+		public String testRequiredValueLegacyAnnotation(
+			@ShellOption(help = ARG1_DESC)
+			String arg1
+		) {
+			return "Hello " + arg1;
+		}
 	}
 
-	@Bean
-	public CommandRegistration testRequiredValueRegistration(CommandRegistration.BuilderSupplier builder) {
-		return builder.get()
-			.command(REG, "required-value")
-			.group(GROUP)
-			.withOption()
-				.longNames("arg1")
-				.description("Desc arg1")
-				.required()
-				.and()
-			.withTarget()
-				.function(ctx -> {
-					String arg1 = ctx.getOptionValue("arg1");
-					return "Hello " + arg1;
-				})
-				.and()
-			.build();
+	@Command(command = BaseE2ECommands.ANNO, group = BaseE2ECommands.GROUP)
+	public static class RequiredValueCommandsAnnotation extends BaseE2ECommands {
+
+		@Command(command = REQUIRED_VALUE)
+		public String testRequiredValueAnnotation(
+			@Option(longNames = ARG1_NAME, required = true, description = ARG1_DESC)
+			String arg1
+		) {
+			return "Hello " + arg1;
+		}
+	}
+
+	@Component
+	public static class RequiredValueCommandsRegistration extends BaseE2ECommands {
+
+		@Bean
+		public CommandRegistration testRequiredValueRegistration(CommandRegistration.BuilderSupplier builder) {
+			return builder.get()
+				.command(REG, REQUIRED_VALUE)
+				.group(GROUP)
+				.withOption()
+					.longNames(ARG1_NAME)
+					.description(ARG1_DESC)
+					.required()
+					.and()
+				.withTarget()
+					.function(ctx -> {
+						String arg1 = ctx.getOptionValue(ARG1_NAME);
+						return "Hello " + arg1;
+					})
+					.and()
+				.build();
+		}
 	}
 }
