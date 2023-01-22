ed2997fe7e7fa7571a8d6999b47e824f45994b9e
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-01-22 09:58:06 +0000

    stash

--- spring-shell-core/src/test/java/org/springframework/shell/command/parser/ParserTests.java
+++ spring-shell-core/src/test/java/org/springframework/shell/command/parser/ParserTests.java
@@ -38,171 +38,190 @@ class ParserTests extends AbstractParsingTests {
 		assertThat(result.messageResults()).isEmpty();
 	}
 
-	@Test
-	void shouldFindOption() {
-		register(ROOT3);
-		ParseResult result = parse("root3", "--arg1");
-		assertThat(result).isNotNull();
-		assertThat(result.commandRegistration()).isNotNull();
-		assertThat(result.optionResults()).isNotEmpty();
-		assertThat(result.messageResults()).isEmpty();
+	@Nested
+	class CommandArguments {
+
+		@Test
+		void commandArgumentsGetsAddedWhenNoOptions() {
+			register(ROOT1);
+			ParseResult result = parse("root1", "arg1", "arg2");
+
+			assertThat(result.argumentResults()).satisfiesExactly(
+				ar -> {
+					assertThat(ar.value()).isEqualTo("arg1");
+					assertThat(ar.position()).isEqualTo(1);
+				},
+				ar -> {
+					assertThat(ar.value()).isEqualTo("arg2");
+					assertThat(ar.position()).isEqualTo(2);
+				}
+			);
+		}
+
+		@Test
+		void commandArgumentsGetsAddedAfterDoubleDash() {
+			register(ROOT3);
+			ParseResult result = parse("root3", "--arg1", "value1", "--", "arg1", "arg2");
+
+			assertThat(result.argumentResults()).satisfiesExactly(
+				ar -> {
+					assertThat(ar.value()).isEqualTo("arg1");
+					assertThat(ar.position()).isEqualTo(4);
+				},
+				ar -> {
+					assertThat(ar.value()).isEqualTo("arg2");
+					assertThat(ar.position()).isEqualTo(5);
+				}
+			);
+		}
 	}
 
-	@Test
-	void shouldFindOptionArgument() {
-		register(ROOT3);
-		ParseResult result = parse("root3", "--arg1", "value1");
-		assertThat(result).isNotNull();
-		assertThat(result.commandRegistration()).isNotNull();
-		assertThat(result.optionResults()).isNotEmpty();
-		assertThat(result.optionResults().get(0).value()).isEqualTo("value1");
-		assertThat(result.messageResults()).isEmpty();
+	@Nested
+	class TypeConversions {
+
+		@Test
+		void optionValueShouldBeInteger() {
+			register(ROOT6_OPTION_INT);
+			ParseResult result = parse("root6", "--arg1", "1");
+			assertThat(result).isNotNull();
+			assertThat(result.commandRegistration()).isNotNull();
+			assertThat(result.optionResults()).isNotEmpty();
+			assertThat(result.optionResults().get(0).value()).isEqualTo(1);
+			assertThat(result.messageResults()).isEmpty();
+		}
+
+		@Test
+		void optionValueShouldBeIntegerArray() {
+			register(ROOT6_OPTION_INTARRAY);
+			ParseResult result = parse("root6", "--arg1", "1", "2");
+			assertThat(result).isNotNull();
+			assertThat(result.commandRegistration()).isNotNull();
+			assertThat(result.optionResults()).isNotEmpty();
+			assertThat(result.optionResults().get(0).value()).isEqualTo(new int[] { 1, 2 });
+			assertThat(result.messageResults()).isEmpty();
+		}
+
+		@Test
+		void optionValueFailsFromStringToInteger() {
+			register(ROOT6_OPTION_INT);
+			ParseResult result = parse("root6", "--arg1", "x");
+			assertThat(result).isNotNull();
+			assertThat(result.commandRegistration()).isNotNull();
+			assertThat(result.optionResults()).isNotEmpty();
+			assertThat(result.optionResults().get(0).value()).isEqualTo("x");
+			assertThat(result.messageResults()).isNotEmpty();
+			assertThat(result.messageResults()).satisfiesExactly(ms -> {
+				// "2002E:(pos 0): Illegal option value 'x', reason 'Failed to convert from type [java.lang.String] to type [int] for value 'x''"
+				assertThat(ms.getMessage()).contains("Failed to convert");
+			});
+		}
 	}
 
-	@Test
-	void directiveNoValueWithCommand() {
-		register(ROOT3);
-		ParserConfiguration configuration = new ParserConfiguration().setEnableDirectives(true);
+	@Nested
+	class ParserMessages {
 
-		ParseResult result = parse(lexer(configuration), "[fake]", "root3");
-		assertThat(result.directiveResults()).satisfiesExactly(d -> {
-			assertThat(d.name()).isEqualTo("fake");
-			assertThat(d.value()).isNull();
-		});
-		assertThat(result.messageResults()).isEmpty();
+		@Test
+		void shouldHaveErrorResult() {
+			register(ROOT4);
+			ParseResult result = parse("root4");
+			assertThat(result).isNotNull();
+			assertThat(result.messageResults()).satisfiesExactly(message -> {
+				ParserAssertions.assertThat(message.parserMessage()).hasCode(2000).hasType(ParserMessage.Type.ERROR);
+			});
+			// "100E:(pos 0): Missing option, longnames='arg1', shortnames=''"
+			assertThat(result.messageResults().get(0).getMessage()).contains("Missing option", "arg1");
+		}
+
+		@Test
+		void shouldHaveErrorResult2() {
+			register(ROOT4);
+			// ParseResult result = parse("root4", "--arg1", "value1", "--arg2", "value2");
+			ParseResult result = parse("root4", "--arg1", "--arg2");
+			assertThat(result).isNotNull();
+			assertThat(result.messageResults()).satisfiesExactly(message -> {
+				ParserAssertions.assertThat(message.parserMessage()).hasCode(2001).hasType(ParserMessage.Type.ERROR);
+			});
+			// "101E:(pos 0): Unrecognised option '--arg2'"
+			// assertThat(result.messageResults().get(0).getMessage()).contains("xxx");
+		}
+
+		@Test
+		void lexerMessageShouldGetPropagated() {
+			ParseResult parse = parse("--");
+			assertThat(parse.messageResults()).extracting(ms -> ms.parserMessage().getCode()).containsExactly(1000, 1000);
+		}
 	}
 
-	@Test
-	void directiveWithValueWithCommand() {
-		register(ROOT3);
-		ParserConfiguration configuration = new ParserConfiguration().setEnableDirectives(true);
+	@Nested
+	class Directives {
 
-		ParseResult result = parse(lexer(configuration), "[fake:value]", "root3");
-		assertThat(result.directiveResults()).satisfiesExactly(d -> {
-			assertThat(d.name()).isEqualTo("fake");
-			assertThat(d.value()).isEqualTo("value");
-		});
-		assertThat(result.messageResults()).isEmpty();
-	}
+		@Test
+		void directiveNoValueWithCommand() {
+			register(ROOT3);
+			ParserConfiguration configuration = new ParserConfiguration().setEnableDirectives(true);
 
-	@Test
-	void multipleDirectivesWithCommand() {
-		register(ROOT3);
-		ParserConfiguration configuration = new ParserConfiguration().setEnableDirectives(true);
-
-		ParseResult result = parse(lexer(configuration), "[foo][bar:value]", "root3");
-		assertThat(result.directiveResults()).satisfiesExactly(
-			d -> {
-				assertThat(d.name()).isEqualTo("foo");
+			ParseResult result = parse(lexer(configuration), "[fake]", "root3");
+			assertThat(result.directiveResults()).satisfiesExactly(d -> {
+				assertThat(d.name()).isEqualTo("fake");
 				assertThat(d.value()).isNull();
-			},
-			d -> {
-				assertThat(d.name()).isEqualTo("bar");
+			});
+			assertThat(result.messageResults()).isEmpty();
+		}
+
+		@Test
+		void directiveWithValueWithCommand() {
+			register(ROOT3);
+			ParserConfiguration configuration = new ParserConfiguration().setEnableDirectives(true);
+
+			ParseResult result = parse(lexer(configuration), "[fake:value]", "root3");
+			assertThat(result.directiveResults()).satisfiesExactly(d -> {
+				assertThat(d.name()).isEqualTo("fake");
 				assertThat(d.value()).isEqualTo("value");
 			});
-		assertThat(result.messageResults()).isEmpty();
+			assertThat(result.messageResults()).isEmpty();
+		}
+
+		@Test
+		void multipleDirectivesWithCommand() {
+			register(ROOT3);
+			ParserConfiguration configuration = new ParserConfiguration().setEnableDirectives(true);
+
+			ParseResult result = parse(lexer(configuration), "[foo][bar:value]", "root3");
+			assertThat(result.directiveResults()).satisfiesExactly(
+				d -> {
+					assertThat(d.name()).isEqualTo("foo");
+					assertThat(d.value()).isNull();
+				},
+				d -> {
+					assertThat(d.name()).isEqualTo("bar");
+					assertThat(d.value()).isEqualTo("value");
+				});
+			assertThat(result.messageResults()).isEmpty();
+		}
 	}
 
-	@Test
-	void shouldHaveErrorResult() {
-		register(ROOT4);
-		ParseResult result = parse("root4");
-		assertThat(result).isNotNull();
-		assertThat(result.messageResults()).satisfiesExactly(message -> {
-			ParserAssertions.assertThat(message.parserMessage()).hasCode(2000).hasType(ParserMessage.Type.ERROR);
-		});
-		// "100E:(pos 0): Missing option, longnames='arg1', shortnames=''"
-		assertThat(result.messageResults().get(0).getMessage()).contains("Missing option", "arg1");
-	}
+	class LongOptions {
 
-	@Test
-	void shouldHaveErrorResult2() {
-		register(ROOT4);
-		// ParseResult result = parse("root4", "--arg1", "value1", "--arg2", "value2");
-		ParseResult result = parse("root4", "--arg1", "--arg2");
-		assertThat(result).isNotNull();
-		assertThat(result.messageResults()).satisfiesExactly(message -> {
-			ParserAssertions.assertThat(message.parserMessage()).hasCode(2001).hasType(ParserMessage.Type.ERROR);
-		});
-		// "101E:(pos 0): Unrecognised option '--arg2'"
-		// assertThat(result.messageResults().get(0).getMessage()).contains("xxx");
-	}
+		@Test
+		void shouldFindLongOption() {
+			register(ROOT3);
+			ParseResult result = parse("root3", "--arg1");
+			assertThat(result).isNotNull();
+			assertThat(result.commandRegistration()).isNotNull();
+			assertThat(result.optionResults()).isNotEmpty();
+			assertThat(result.messageResults()).isEmpty();
+		}
 
-	@Test
-	void lexerMessageShouldGetPropagated() {
-		ParseResult parse = parse("--");
-		assertThat(parse.messageResults()).extracting(ms -> ms.parserMessage().getCode()).containsExactly(1000, 1000);
-	}
-
-	@Test
-	void optionValueShouldBeInteger() {
-		register(ROOT6_OPTION_INT);
-		ParseResult result = parse("root6", "--arg1", "1");
-		assertThat(result).isNotNull();
-		assertThat(result.commandRegistration()).isNotNull();
-		assertThat(result.optionResults()).isNotEmpty();
-		assertThat(result.optionResults().get(0).value()).isEqualTo(1);
-		assertThat(result.messageResults()).isEmpty();
-	}
-
-	@Test
-	void optionValueShouldBeIntegerArray() {
-		register(ROOT6_OPTION_INTARRAY);
-		ParseResult result = parse("root6", "--arg1", "1", "2");
-		assertThat(result).isNotNull();
-		assertThat(result.commandRegistration()).isNotNull();
-		assertThat(result.optionResults()).isNotEmpty();
-		assertThat(result.optionResults().get(0).value()).isEqualTo(new int[] { 1, 2 });
-		assertThat(result.messageResults()).isEmpty();
-	}
-
-	@Test
-	void optionValueFailsFromStringToInteger() {
-		register(ROOT6_OPTION_INT);
-		ParseResult result = parse("root6", "--arg1", "x");
-		assertThat(result).isNotNull();
-		assertThat(result.commandRegistration()).isNotNull();
-		assertThat(result.optionResults()).isNotEmpty();
-		assertThat(result.optionResults().get(0).value()).isEqualTo("x");
-		assertThat(result.messageResults()).isNotEmpty();
-		assertThat(result.messageResults()).satisfiesExactly(ms -> {
-			// "2002E:(pos 0): Illegal option value 'x', reason 'Failed to convert from type [java.lang.String] to type [int] for value 'x''"
-			assertThat(ms.getMessage()).contains("Failed to convert");
-		});
-	}
-
-	@Test
-	void commandArgumentsGetsAddedWhenNoOptions() {
-		register(ROOT1);
-		ParseResult result = parse("root1", "arg1", "arg2");
-
-		assertThat(result.argumentResults()).satisfiesExactly(
-			ar -> {
-				assertThat(ar.value()).isEqualTo("arg1");
-				assertThat(ar.position()).isEqualTo(1);
-			},
-			ar -> {
-				assertThat(ar.value()).isEqualTo("arg2");
-				assertThat(ar.position()).isEqualTo(2);
-			}
-		);
-	}
-
-	@Test
-	void commandArgumentsGetsAddedAfterDoubleDash() {
-		register(ROOT3);
-		ParseResult result = parse("root3", "--arg1", "value1", "--", "arg1", "arg2");
-
-		assertThat(result.argumentResults()).satisfiesExactly(
-			ar -> {
-				assertThat(ar.value()).isEqualTo("arg1");
-				assertThat(ar.position()).isEqualTo(4);
-			},
-			ar -> {
-				assertThat(ar.value()).isEqualTo("arg2");
-				assertThat(ar.position()).isEqualTo(5);
-			}
-		);
+		@Test
+		void shouldFindLongOptionArgument() {
+			register(ROOT3);
+			ParseResult result = parse("root3", "--arg1", "value1");
+			assertThat(result).isNotNull();
+			assertThat(result.commandRegistration()).isNotNull();
+			assertThat(result.optionResults()).isNotEmpty();
+			assertThat(result.optionResults().get(0).value()).isEqualTo("value1");
+			assertThat(result.messageResults()).isEmpty();
+		}
 	}
 
 	@Nested
