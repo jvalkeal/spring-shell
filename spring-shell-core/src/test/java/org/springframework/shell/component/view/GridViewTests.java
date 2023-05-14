047d4084f6111e67ffbce4357911ee404647a58e
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-05-14 16:04:13 +0100

    stash

--- spring-shell-core/src/test/java/org/springframework/shell/component/view/GridViewTests.java
+++ spring-shell-core/src/test/java/org/springframework/shell/component/view/GridViewTests.java
@@ -17,7 +17,6 @@ package org.springframework.shell.component.view;
 
 import org.assertj.core.api.AssertProvider;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
