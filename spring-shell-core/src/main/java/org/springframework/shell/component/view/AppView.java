d9b5cf0bccef4f4dcca20b9ae9c839a8254b3ec7
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-06-01 14:08:08 +0100

    stash

---
+++ spring-shell-core/src/main/java/org/springframework/shell/component/view/AppView.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.shell.component.view;
+
+import org.springframework.shell.component.view.geom.Rectangle;
+import org.springframework.shell.component.view.screen.Screen;
+
+public class AppView extends BoxView {
+
+	private View main;
+	private View modal;
+
+	@Override
+	protected void drawInternal(Screen screen) {
+		Rectangle rect = getInnerRect();
+		if (main != null) {
+			main.setRect(rect.x(), rect.y(), rect.width(), rect.height());
+			main.draw(screen);
+		}
+		if (modal != null) {
+			modal.setRect(rect.x() + 5, rect.y() + 5, rect.width() - 10, rect.height() - 10);
+			modal.draw(screen);
+		}
+		super.drawInternal(screen);
+	}
+
+	public void setMain(View main) {
+		this.main = main;
+	}
+
+	public void setModal(View modal) {
+		this.modal = modal;
+	}
+}
