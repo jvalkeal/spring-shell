c6d3528e822b5b39b0f86d7825377527a4e0f910
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-06-02 07:51:27 +0100

    stash

--- e2e/spring-shell-e2e-tests/src/utils.ts
+++ e2e/spring-shell-e2e-tests/src/utils.ts
@@ -4,11 +4,11 @@ import * as path from 'path';
 export const tempDir = path.join(__dirname, 'spring-shell', 'temp');
 export const isWindows = os.platform() === 'win32';
 export const cliPathRelative = isWindows
-  ? '..\\..\\spring-shell-samples\\build\\native\\nativeCompile\\spring-shell-samples.exe'
-  : '../../spring-shell-samples/build/native/nativeCompile/spring-shell-samples';
+  ? '..\\..\\spring-shell-samples\\spring-shell-sample-e2e\\build\\native\\nativeCompile\\spring-shell-sample-e2e.exe'
+  : '../../spring-shell-samples/spring-shell-sample-e2e/build/native/nativeCompile/spring-shell-sample-e2e';
 export const jarPathRelative = isWindows
-  ? '..\\..\\spring-shell-samples\\build\\libs\\spring-shell-samples.jar'
-  : '../../spring-shell-samples/build/libs/spring-shell-samples.jar';
+  ? '..\\..\\spring-shell-samples\\spring-shell-sample-e2e\\build\\libs\\spring-shell-sample-e2e.jar'
+  : '../../spring-shell-samples/spring-shell-sample-e2e/build/libs/spring-shell-sample-e2e.jar';
 export const cliPath = path.resolve(cliPathRelative);
 export const jarPath = path.resolve(jarPathRelative);
 export const nativeDesc = 'native';
