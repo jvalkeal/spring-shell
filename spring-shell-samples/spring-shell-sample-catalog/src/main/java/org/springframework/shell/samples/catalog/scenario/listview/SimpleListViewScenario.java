a4041e41d65eac4860c31b28af6a72c2a640d374
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-07-07 17:01:00 +0100

    stash

--- spring-shell-samples/spring-shell-sample-catalog/src/main/java/org/springframework/shell/samples/catalog/scenario/listview/SimpleListViewScenario.java
+++ spring-shell-samples/spring-shell-sample-catalog/src/main/java/org/springframework/shell/samples/catalog/scenario/listview/SimpleListViewScenario.java
@@ -25,7 +25,7 @@ import org.springframework.shell.samples.catalog.scenario.ScenarioComponent;
 import static org.springframework.shell.samples.catalog.scenario.Scenario.CATEGORY_LISTVIEW;
 
 @ScenarioComponent(name = "Basic", description = "Basic list", category = { CATEGORY_LISTVIEW })
-public class SimpleListView extends AbstractScenario {
+public class SimpleListViewScenario extends AbstractScenario {
 
 	@Override
 	public View build() {
