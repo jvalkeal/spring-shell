e78b1bdef042f1615ee62202116b0b4279926489
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-08-18 06:59:43 +0100

    stash

--- buildSrc/src/main/java/org/springframework/shell/gradle/JavaConventions.java
+++ buildSrc/src/main/java/org/springframework/shell/gradle/JavaConventions.java
@@ -59,6 +59,7 @@ class JavaConventions {
 	}
 
 	private void configureJavaConventions(Project project) {
+		System.out.println("XXX1 " + JavaVersion.current());
 		project.getTasks().withType(JavaCompile.class, (compile) -> {
 			compile.getOptions().setEncoding("UTF-8");
 			List<String> args = compile.getOptions().getCompilerArgs();
