e7b8f04db55a54b99a6331acb6b3fe7906ccaa7c
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-08-26 15:04:07 +0100

    stash

--- scripts/generate-languages.sh
+++ scripts/generate-languages.sh
@@ -17,6 +17,28 @@ tree-sitter,tree-sitter-json,v0.21.0,json
 tree-sitter-grammars,tree-sitter-yaml,v0.6.1,yaml
 '
 
+# tree-sitter,tree-sitter-python,v0.21.0,python
+# tree-sitter,tree-sitter-php,v0.22.8,php
+# tree-sitter,tree-sitter-cpp,v0.22.3,cpp
+# tree-sitter,tree-sitter-ruby,v0.21.0,ruby
+# tree-sitter,tree-sitter-c,v0.21.4,c
+# tree-sitter,tree-sitter-scala,v0.22.5,scala
+# tree-sitter,tree-sitter-go,v0.21.2,go
+# tree-sitter,tree-sitter-rust,v0.21.2,rust
+# tree-sitter,tree-sitter-javascript,v0.21.4,javascript
+# tree-sitter,tree-sitter-html,v0.20.4,html
+# tree-sitter,tree-sitter-css,v0.21.1,css
+# tree-sitter,tree-sitter-julia,v0.22.6,julia
+# tree-sitter,tree-sitter-typescript,v0.21.2,typescript
+# tree-sitter,tree-sitter-jsdoc,v0.21.0,jsdoc
+# tree-sitter,tree-sitter-regex,v1.0.0,regex
+# tree-sitter,tree-sitter-ocaml,v0.22.0,ocaml
+# tree-sitter,tree-sitter-c-scharp,v0.21.3,csharp
+# tree-sitter,tree-sitter-bash,v0.21.0,bash
+# tree-sitter-grammars,tree-sitter-markdown,v0.2.3,markdown
+# tree-sitter-grammars,tree-sitter-xml,v0.6.4,xml
+# tree-sitter-grammars,tree-sitter-lua,v0.1.0,lua
+
 export PROJECTBASEDIR=$(find_basedir)
 
 for VALUE in $VALUES;
